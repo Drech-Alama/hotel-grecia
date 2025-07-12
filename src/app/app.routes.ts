ce384d1f9916ad4d68392a4945e8c493072f586e
Andres Alama <alama33drech33@gmail.com>
2025-07-12 01:06:58 -0500

    Primer commit - Proyecto Angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { UsComponent } from './pages/us/us.component';
+import { ServicesComponent } from './pages/services/services.component';
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'nosotros', component: UsComponent },
+  { path: 'servicios', component: ServicesComponent }
+];
---
+++ src/app/shared/header/header.component.ts
@@ -0,0 +1,21 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  imports: [CommonModule, RouterModule
+  ],
+  templateUrl: './header.component.html',
+})
+export class HeaderComponent {
+  isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu() {
+    this.isMenuOpen = false;
+  }
+}
